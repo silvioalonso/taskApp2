509c32e042b310123730c3130188c93555c2cb2f
Nilolive <nilolive@gmail.com>
2015-03-27 17:55:24 -0300

    Menu navegacao app / console

--- js/registro.js
+++ js/registro.js
@@ -1,70 +1,70 @@
-var btnChange = true;
-var arrTarefas = [];
-var arr = [];
+var btnDelete = true;
+var arrRegistros = [];
 var store;
 var grid
 var emptyStore;
 var updateItem=false;
 var selectedItem;
 var timeFormat = 'DD/MM/YYYY, H:mm:ss';
-
-//Tratar objeto data hora
-var strIni=eval(localStorage.getItem("tempos"))[0].inicio.split(",")[0];
-var strHrInicio=eval(localStorage.getItem("tempos"))[0].inicio.split(",")[1];
-
-var strFim=eval(localStorage.getItem("tempos"))[0].fim.split(",")[0];
-var strHrFim=eval(localStorage.getItem("tempos"))[0].fim.split(",")[1];
-
-var diaI;
-var mesI;
-var anoI;
-
-for (var d=0;d<strIni.length;d++){
-  diaI=strIni[d].split("/");
-  mesI=strIni[d].split("/");
-  anoI=strIni[d].split("/");
-}
-
-var horaI;
-var minutosI;
-var segundosI;
-
-for (var h=0;h<strHrInicio.length;h++){
-  horaI=strHrInicio[h].split("/");
-  minutosI=strHrInicio[h].split("/");
-  segundosI=strHrInicio[h].split("/");
-}
-
-var dataHoraIni = new Date(anoI, mesI, diaI, horaI, minutosI, segundosI,null); 
-
-var diaF;
-var mesF;
-var anoF;
-
-for (var f=0;f<strFim.length;f++){
-  diaF=strFim[f].split("/");
-  mesF=strFim[f].split("/");
-  anoF=strFim[f].split("/");
-}
-
-var horaF;
-var minutosF;
-var segundosF;
-
-for (var u=0;u<strHrFim.length;u++){
-  horaF=strHrFim[u].split("/");
-  minutosF=strHrFim[u].split("/");
-  segundosF=strHrFim[u].split("/");
-}
-
-var dataHoraFim = new Date(anoF, mesF, diaF, horaF, minutosF, segundosF,null);
-
 var timeDiff=new Date();
 
-timeDiff = dataHoraFim - dataHoraIni;
-timeDiff /= 1000;
+function formataDataHora(){
 
-moment(timeDiff).format(timeFormat) ;
+      //Tratar objeto data hora
+    var strIni=eval(localStorage.getItem("tempos"))[0].inicio.split(",")[0];
+    var strHrInicio=eval(localStorage.getItem("tempos"))[0].inicio.split(",")[1];
+
+    var strFim=eval(localStorage.getItem("tempos"))[0].fim.split(",")[0];
+    var strHrFim=eval(localStorage.getItem("tempos"))[0].fim.split(",")[1];
+
+    var diaI;
+    var mesI;
+    var anoI;
+
+    for (var d=0;d<strIni.length;d++){
+      diaI=strIni[d].split("/");
+      mesI=strIni[d].split("/");
+      anoI=strIni[d].split("/");
+    }
+
+    var horaI;
+    var minutosI;
+    var segundosI;
+
+    for (var h=0;h<strHrInicio.length;h++){
+      horaI=strHrInicio[h].split("/");
+      minutosI=strHrInicio[h].split("/");
+      segundosI=strHrInicio[h].split("/");
+    }
+
+    var dataHoraIni = new Date(anoI, mesI, diaI, horaI, minutosI, segundosI,null); 
+
+    var diaF;
+    var mesF;
+    var anoF;
+
+    for (var f=0;f<strFim.length;f++){
+      diaF=strFim[f].split("/");
+      mesF=strFim[f].split("/");
+      anoF=strFim[f].split("/");
+    }
+
+    var horaF;
+    var minutosF;
+    var segundosF;
+
+    for (var u=0;u<strHrFim.length;u++){
+      horaF=strHrFim[u].split("/");
+      minutosF=strHrFim[u].split("/");
+      segundosF=strHrFim[u].split("/");
+    }
+
+    var dataHoraFim = new Date(anoF, mesF, diaF, horaF, minutosF, segundosF,null);
+  
+    timeDiff = (dataHoraFim) - (dataHoraIni);
+    moment(timeDiff).format(timeFormat) ;
+
+}
 
   var tempos = {
     nome:"",
@@ -73,9 +73,37 @@ moment(timeDiff).format(timeFormat) ;
     tempogasto: timeDiff
   }
 
-  
+formataDataHora();
 
-  function preencheGridTarefas(){
+function salvaRegistros(nome,inicio,fim,tempogasto){   
+
+  if (updateItem){
+          
+    updateItem=false;
+          
+    arrRegistros[selectedItem].nome=dojo.byId("nome").innerText;
+    //arrRegistros[selectedItem].identificador=dojo.byId("identificador").value;
+    arrRegistros[selectedItem].inicio=dojo.byId("inicio").value;
+    arrRegistros[selectedItem].fim=dojo.byId("fim").value;
+    arrRegistros[selectedItem].tempogasto=dojo.byId("tempogasto").value;
+
+    localStorage.setItem("tempos", JSON.stringify(arrRegistros));                                     
+         
+  }else{
+
+    if (localStorage.getItem("tempos") !== null){
+      arrRegistros=eval(localStorage.getItem("tempos"));
+  }
+
+    arrRegistros.push({"nome":nome,"inicio":inicio,"fim":fim,"tempogasto":tempogasto});
+    localStorage.setItem("tempos", JSON.stringify(arrRegistros));     
+
+  }
+  preencheGridRegistros();
+}
+
+
+  function preencheGridRegistros(){
 
     require(['dojo/_base/lang', 'dojo/data/ItemFileWriteStore', 'dojo/dom', 'dojo/domReady!'],
     function(lang, ItemFileWriteStore, dom){
@@ -83,9 +111,9 @@ moment(timeDiff).format(timeFormat) ;
       if (localStorage.getItem("Tarefas") !== null){
 
         var tarefas = localStorage.getItem("tempos");
-        arrTarefas = eval(tarefas);
+        arrRegistros = eval(tarefas);
 
-          if (arrTarefas!=null){
+          if (arrRegistros!=null){
 
             var data = {
               id: "id",
@@ -95,8 +123,8 @@ moment(timeDiff).format(timeFormat) ;
                 
             store = new ItemFileWriteStore({data: data});           
                   
-            for (var i = 0; i < arrTarefas.length; i++) {    
-              var itemTarefa =  { id: i, nome: arrTarefas[i].nome,inicio:arrTarefas[i].inicio,fim:arrTarefas[i].fim,tempogasto:moment(tempos.tempogasto).format(timeFormat).split(",")[1]}; 
+            for (var i = 0; i < arrRegistros.length; i++) {
+                var itemTarefa =  { id: i, nome: arrRegistros[i].nome,inicio:arrRegistros[i].inicio,fim:arrRegistros[i].fim,tempogasto:moment(tempogasto).format(timeFormat).split(",")[1]}; 
                 store.newItem(itemTarefa);                     
             }
            
@@ -126,10 +154,10 @@ require(["dojo/ready"], function(ready){
     /*set up layout*/
     var layout = [[
    
-      {'name': 'Tarefa', 'field': 'nome', 'width': '50%'},
-      {'name': 'Inicio', 'field': 'inicio', 'width': '50%'},
-      {'name': 'Fim', 'field': 'fim', 'width': '50%'},
-      {'name': 'Tempo gasto', 'field': 'tempogasto', 'width': '50%'}
+      {'name': 'Tarefa', 'field': 'nome', 'width': '40%'},
+      {'name': 'Inicio', 'field': 'inicio', 'width': '20%'},
+      {'name': 'Fim', 'field': 'fim', 'width': '20%'},
+      {'name': 'Tempo gasto', 'field': 'tempogasto', 'width': '20%'}
     ]];
 
     /*create a new grid*/
@@ -144,7 +172,7 @@ require(["dojo/ready"], function(ready){
 
       /*Call startup() to render the grid*/
       grid.startup();
-      preencheGridTarefas();
+      preencheGridRegistros();
 
       // assuming our grid is stored in a variable called "myGrid":
       grid.on("RowClick", function(evt){
@@ -152,15 +180,19 @@ require(["dojo/ready"], function(ready){
         rowData = grid.getItem(idx);
         
       
-        dojo.byId("nome").innerText = rowData.nome[0];
-        dojo.byId("inicio").innerText = rowData.inicio[0];
-        dojo.byId("fim").innerText = rowData.fim[0];
-        dojo.byId("tempogasto").innerText = rowData.tempogasto[0];
+       
+        //dojo.byId("inicio").innerText = rowData.inicio[0];
+        //dojo.byId("fim").innerText = rowData.fim[0];
+        //dojo.byId("tempogasto").innerText = rowData.tempogasto[0];
+
+         dojo.byId("nome").value = rowData.nome[0];
+         dojo.byId("inicio").value=rowData.inicio[0];
+         dojo.byId("fim").value=rowData.fim[0];
+         dojo.byId("tempogasto").value=rowData.tempogasto[0];
 
         updateItem=true;
         selectedItem=idx;
               
-        console.debug(rowData);
       }, true);
 
     });
@@ -170,3 +202,78 @@ require(["dojo/ready"], function(ready){
 
 
 
+require(["dojo/ready"], function(ready){
+  ready(function(){
+  
+    require(["dijit/form/Button", "dojo/dom", "dojo/domReady!"], function(Button, dom){
+   
+     var btnSave = new Button({  
+
+        iconClass:'dijitEditorIcon dijitEditorIconSave', 
+        showLabel: false,          
+        onClick: function(){
+                   
+          //var txtId = dojo.byId("identificador").value;
+          var txtNome = dojo.byId("nome").value;
+          var txtInicio = dojo.byId("inicio").value;
+          var txtFim = dojo.byId("fim").value;
+          var txtTempoGasto = dojo.byId("tempogasto").value;
+
+          salvaRegistros(txtNome,txtInicio,txtFim,txtTempoGasto);
+          
+          preencheGridRegistros(); 
+          limpaCampos();                 
+        }
+      }, "btnSave").startup();
+
+      var btnDelete = new Button({  
+
+      iconClass:'dijitEditorIcon dijitEditorIconDelete', 
+      showLabel: false,          
+        onClick: function(){
+
+          updateItem=false;
+          var items = grid.selection.getSelected();
+                                
+          if(items.length){                  
+                   
+            dojo.forEach(items, function(selectedItem){
+            
+            if(selectedItem !== null){
+            
+              store.deleteItem(selectedItem);
+
+              arrTarefas.splice(selectedItem, 1);
+              localStorage.setItem("Tarefas", JSON.stringify(arrTarefas));                                   
+
+            } 
+          });
+        }
+        limpaCampos();            
+      }
+    }, "btnDelete").startup();
+
+    var btnClear = new Button({  
+
+      iconClass:'dijitEditorIcon dijitEditorIconClear', 
+      showLabel: false,          
+        onClick: function(){
+
+          updateItem=false;
+            limpaCampos();                               
+        }
+      }, "btnClear").startup();
+
+  
+    });
+
+  });
+});
+
+function limpaCampos(){
+    var inputs = document.getElementsByTagName('input');
+
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].value = '';
+    }
+}
