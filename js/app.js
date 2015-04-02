1a7424621e1dda3ab943fb0fe0bb6e10a9c0d24c
silvioalonso <silvioalonso@poli.ufrj.br>
2015-04-02 15:54:55 -0300

    funcao hash

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ var btnChange = true;
 var arrTarefas = [];
 var arrTempos = [];
 
+function testesMD5(){
+  console.log($.md5("aiosj"));
+}
+
 function atualizaArray(){
 
   if (localStorage.getItem("Tarefas") !== null){
@@ -21,11 +25,10 @@ function carregaComboBox(arr){
   }
 }
 
-function salvaTempos(tarefa, tInicial, tFinal,id){
+function salvaTempos(tarefa, tInicial, tFinal, id){
   id = id || null;
   var obj = {id: id, nome: tarefa, inicio: tInicial, fim: tFinal};
   arrTempos.push(obj);
-//    localStorage.setItem("tempos", JSON.stringify(arrTempos));
   localStorage.setItem("tempos", JSON.stringify(arrTempos));
 }
 
@@ -33,30 +36,50 @@ function salvaTemposAjax(tarefa, tInicial, tFinal){
   $.ajax({
     type: "POST",
     url:"http://localhost:8080/gko-taskapp-service/registro/salvar",
-    data: {tarefa: tarefa, inicio: tInicial, fim: tFinal}
+    data: {tarefa: tarefa, inicio: tInicial, fim: tFinal, idUsuario: 1, hash: "123"}
 
   }).error(function(){
     salvaTempos(tarefa, tInicial, tFinal);
 
   }).done(function(data){
+    //console.log(data);
+    //id = data.registro.id;
+    //salvaTempos(tarefa, tInicial, tFinal, id);
     console.log(data);
-    id = data.registro.id;
-    salvaTempos(tarefa, tInicial, tFinal, id);
   });
 }
 
 function synchronize(){
   //atualizaArray();
-  console.log("teste");
   for(var i = 0; i < arrTempos.length; i++){
     if(arrTempos[i].id == null){
-      console.log("teste2")
       salvaTemposAjax(arrTempos[i].nome, arrTempos[i].inicio, arrTempos[i].fim);
       arrTempos.splice(i,1);
     }
   }
 }
 
+function editaTempos(i, tInicial, tFinal, sync){
+  arrTempos[i].inicio = tInicial;
+  arrTempos[i].fim = tFinal;
+  arrTempos[i].isSync = sync;
+  localStorage.setItem("tempos", JSON.stringify(arrTempos));
+  console.log(arrTempos);
+}
+
+function editaTemposAjax(i, tInicial, tFinal){
+  $.ajax({
+    type: "POST",
+    url:"http://localhost:8080/gko-taskapp-service/registro/editar",
+    data: {id: arrTempos[i].id, tarefa: arrTempos[i].nome, inicio: tInicial, fim: tFinal}
+
+  }).error(function(){
+    editaTempos(i, tInicial, tFinal, false);
+
+  }).done(function(data){
+    editaTempos(i, tInicial, tFinal, true);
+  });
+}
 
 $(document).ready(function(){
 
@@ -65,6 +88,7 @@ $(document).ready(function(){
   carregaComboBox(arrTarefas);
 
 	$("#controle").click(function(){
+
     if (btnChange == true) {
       $(this).toggleClass("btn-success").toggleClass("btn-danger").text("Finalizar tarefa");
       $("#sel1").prop('disabled', true);
@@ -86,12 +110,15 @@ $(document).ready(function(){
     }
 	});
 
+
   $("#sel1").change(function(){
-      $("#controle").addClass("btn-success").prop("disabled",false).text("Iniciar tarefa");
-      $(".ini , .fim , #time").hide();
+    $("#controle").addClass("btn-success").prop("disabled",false).text("Iniciar tarefa");
+    $(".ini , .fim , #time").hide();
   });
+
 
   $("#sync").click(function(){
     synchronize();
   });
+
 });
